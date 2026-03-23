affe9d8d43e5e80821fba91d6e0ff4306485a715
Leandro Bolzon <leosilva1711@gmail.com>
2026-03-23 19:18:33 -0300

    Proyecto agregado al repo

---
+++ Gestion de productos/Gestion de productos/Controllers/ProductosController.cs
@@ -0,0 +1,72 @@
+﻿using Gestion_de_productos.Data;
+using Gestion_de_productos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Gestion_de_productos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> Get()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Producto>> Get(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+
+            if (producto == null)
+                return NotFound();
+
+            return producto;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+
+            return Ok(producto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Producto producto)
+        {
+            if (id != producto.Id)
+                return BadRequest();
+
+            _context.Entry(producto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+
+            if (producto == null)
+                return NotFound();
+
+            _context.Productos.Remove(producto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Gestion de productos/Gestion de productos/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Gestion_de_productos.Models;
+
+namespace Gestion_de_productos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos => Set<Producto>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>()
+                .Property(p => p.Precio)
+                .HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
