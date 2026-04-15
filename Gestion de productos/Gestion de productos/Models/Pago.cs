bd3151c758c8f170bce15447d420ddabafbe9565
Leandro Bolzon <leosilva1711@gmail.com>
2026-04-15 18:23:07 -0300

    .

---
+++ Gestion de productos/Gestion de productos/Models/Pago.cs
@@ -0,0 +1,15 @@
+﻿namespace Gestion_de_productos.Models
+{
+    public class Pago
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public string FormaPago { get; set; } = string.Empty;
+        public DateTime FechaPago { get; set; }
+        public string Estado { get; set; } = string.Empty;
+
+
+        // Relación con Pedido
+        public Pedido Pedido { get; set; }
+    }
+}
---
+++ Gestion de productos/Gestion de productos/Models/PedidoDetalle.cs
@@ -0,0 +1,17 @@
+﻿namespace Gestion_de_productos.Models
+{
+    public class PedidoDetalle
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public int PedidoId { get; set; }
+        public int Cantidad { get; set; }
+        public double PrecioUnitario { get; set; }
+        
+        // Relación con Pedido
+        public Pedido Pedido { get; set; }
+        
+        // Relación con Producto
+        public Producto Producto { get; set; }
+    }
+}
